fcd9faac74d2d28095dd063ab7f65a226dfe2f74
Abhinn <abhinn.aakash@gmail.com>
2021-07-19 10:45:36 +0530

    Rough skeleton: wallet connection

--- FrontEndApp/src/modules/Dashboard/Dashboard.tsx
+++ FrontEndApp/src/modules/Dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import { Button, Typography, withStyles } from "@material-ui/core";
 import { styles } from "./Dashboard.styles";
 import { IClasses } from "../shared/interfaces";
 import { RouteComponentProps } from "react-router";
+import Web3 from "web3";
 
 import { observer } from "mobx-react";
 import { ModalStore } from "../shared";
@@ -29,6 +30,38 @@ class Dashboard extends Component<Props> {
     );
   }
 
+  componentDidMount() {
+    this.connect();
+  }
+
+  async connect(): Promise<any> {
+    if (!window.ethereum) {
+      alert("connect to metamax first !");
+      return;
+    }
+
+    window.ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((s: any) => {
+        debugger;
+      })
+      .catch((err: any) => {
+        debugger;
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error// If this happens, the user rejected the connection request.
+          console.log("Please connect to MetaMask.");
+        } else {
+          console.error(err);
+        }
+      });
+    return;
+  }
+
+  async getChainId() {
+    const chainId = await window.ethereum.request({ method: "eth_chainId" });
+    console.log(chainId);
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -36,7 +69,7 @@ class Dashboard extends Component<Props> {
       <>
         <div className={classes.root}>
           <Typography variant="h4">Dashboard</Typography>
-          <Button onClick={() => this.openDialog()} color="primary">
+          <Button onClick={() => this.getChainId()} color="primary">
             click here
           </Button>
         </div>
